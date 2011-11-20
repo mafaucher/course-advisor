c310c82bb057c02128c75a331a366c4fc645489b
ma.faucher <ma.faucher@5137d2d6-d956-9ffb-f55b-46434d082e77>
2011-11-20 20:18:52 +0000

    Added StudentRecord.option

--- src/main/StudentRecord.java
+++ src/main/StudentRecord.java
@@ -2,8 +2,14 @@ package main;
 
 public class StudentRecord 
 {
+    public enum Option
+    {
+        GAME, WEB, CSYS, SSYS, ISYS, MINOR
+    }
+
 	private String name;
 	private long id;
+    private Option option;
 	private float gpa, lastAnnualGpa;
 	private Course[] courses, coursesTaken;
 }
