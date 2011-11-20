6a4d1244173f1e739da57dabf4e7aa52a5ec8ea1
arsenicltd@hotmail.com <arsenicltd@hotmail.com@5137d2d6-d956-9ffb-f55b-46434d082e77>
2011-11-20 01:50:06 +0000

    Added the COMP 474 java project files

---
+++ trunk/src/panels/LoginPanel.java
@@ -0,0 +1,60 @@
+package panels;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class LoginPanel extends ViewPanel 
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField nameField, passwordField;
+	private JButton loginButton, exitButton;
+	public LoginPanel()
+	{
+		ActionListener al=new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+			}
+			
+		};
+		nameField=new JTextField("", 16);
+		passwordField=new JTextField("", 16);
+		loginButton=createButton("Login", al);
+		exitButton=createButton("Exit", null);
+		
+		GroupLayout layout=new GroupLayout(this);
+		setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		layout.setHorizontalGroup(
+				   layout.createSequentialGroup()
+				      	
+				      	.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				      			.addComponent(nameField)
+						      	.addComponent(passwordField)
+						      	.addGroup(layout.createSequentialGroup()
+						      		.addComponent(loginButton)
+						      		.addComponent(exitButton)))
+				);
+				layout.setVerticalGroup(
+				   layout.createSequentialGroup()
+				   	  	.addComponent(nameField)
+				   	  	.addComponent(passwordField)
+				   	  	.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    		  .addComponent(exitButton)
+				    		  .addComponent(loginButton))				      
+				);
+		//add(nameField, BorderLayout.NORTH);
+		//add(passwordField, BorderLayout.CENTER);
+		//addButton("Login", al, BorderLayout.CENTER);
+		//addButton("Exit", null, BorderLayout.CENTER);
+	}
+	
+}
