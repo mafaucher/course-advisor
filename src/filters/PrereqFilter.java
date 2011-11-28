f981ee14b7b19e698c665c250ec2bd1cb4e62e5e
ma.faucher <ma.faucher@5137d2d6-d956-9ffb-f55b-46434d082e77>
2011-11-28 15:28:29 +0000

    Implemented the PrereqFilter and LevelFilter

--- src/filters/PrereqFilter.java
+++ src/filters/PrereqFilter.java
@@ -1,13 +1,24 @@
 package filters;
 
 import main.Course;
+import main.MainFrame;
 
+/**
+ * Binary filter, sets score to 0 if prereqs not satisfied,
+ * Returns score if prereqs are satisfied.
+ */
 public class PrereqFilter implements IFilter {
 
 	@Override
 	public double processScore(Course course, double score) {
-		// TODO Auto-generated method stub
-		return 0;
+	    for (Course prereq : course.getPrereqs())
+	    {
+	        if (!prereq.wasTaken())
+	        {
+	            if ( !MainFrame.getModel().getRecord().isRegistered(prereq) )
+	                return 0;
+	        }
+	    }
+	    return score;
 	}
-
 }
