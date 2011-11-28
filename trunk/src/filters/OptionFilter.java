407b0dbb8585b5127c6688dc924bae373fb6b63c
ma.faucher <ma.faucher@5137d2d6-d956-9ffb-f55b-46434d082e77>
2011-11-28 16:44:36 +0000

    partial progress

---
+++ trunk/src/filters/OptionFilter.java
@@ -0,0 +1,15 @@
+package filters;
+
+import main.Course;
+import main.MainFrame;
+
+public class OptionFilter implements IFilter {
+
+    @Override
+    public double processScore(Course course, double score) {
+        
+        
+        return 0;
+    }
+
+}
